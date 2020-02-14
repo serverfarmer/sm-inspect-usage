4fab5b5e81eef6bbf517e261b83eaff16189bb29
Tomasz Klim <github@tomaszklim.pl>
2020-02-14 01:40:09 +0100

    added cloud.hosts and container.hosts files

--- utils/get-hosts.sh
+++ utils/get-hosts.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 path=/etc/local/.farm
-cat $path/virtual.hosts $path/physical.hosts $path/workstation.hosts |grep -vxFf $path/openvz.hosts |grep -v "^#"
+cat $path/virtual.hosts $path/physical.hosts $path/cloud.hosts $path/container.hosts $path/workstation.hosts |grep -v "^#"
